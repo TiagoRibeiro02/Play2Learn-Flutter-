b1269f3ca281b146531fcc63d81abe9db42de75f
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-09-07 18:23:44 +0100

    move with keyboard

--- play2learn/lib/Pages/login.dart
+++ play2learn/lib/Pages/login.dart
@@ -63,97 +63,98 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin{
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //logo
-                Lottie.asset(
-                    'assets/animation_brain.json',
-                  repeat: false,
-                  height: 250,
-                  width: 250,
-                  animate: false
-                ),
-
-                const SizedBox(height: 40,),
-
-                //welcome back message
-                Text(
-                  "Welcome back, you've been missed!",
-                  style: TextStyle(
-                    color: Colors.grey[700],
+          child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //logo
+                  Lottie.asset(
+                      'assets/animation_brain.json',
+                    repeat: false,
+                    height: 250,
+                    width: 250,
+                    animate: false
                   ),
-                ),
 
-                const SizedBox(height: 25,),
+                  const SizedBox(height: 40,),
 
-                //email textfield
-                MyTextField(
-                    controller: userTextController,
-                    hintText: 'Email',
-                    obscureText: false,
-                    icon: Icon(Icons.nat),
-                    color: Colors.transparent,
-                    onTap: () {},
-                ),
-
-                const SizedBox(height: 10,),
-
-                //password textfield
-                MyTextField(
-                    controller: passwordController,
-                    hintText: 'Password',
-                    obscureText: obstext,
-                    icon: Icon(Icons.remove_red_eye),
-                    color: Colors.grey,
-                    onTap: () {
-                      setState(() {
-                        obstext = !obstext;
-                      });
-                    },
-                ),
-
-                const SizedBox(height: 25,),
-
-                //singin button
-                MyButton(
-                    onTap: singIn,
-                    text: 'Sing In'),
-
-                const SizedBox(height: 10,),
-
-                //go to register page
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Not a member?',
-                      style: TextStyle(
-                          color: Colors.grey[700]
-                      ),
+                  //welcome back message
+                  Text(
+                    "Welcome back, you've been missed!",
+                    style: TextStyle(
+                      color: Colors.grey[700],
                     ),
-                    const SizedBox(width: 4,),
-                    GestureDetector(
-                      onTap: widget.onTap,
-                      child: const Text(
-                        'Register Now',
+                  ),
+
+                  const SizedBox(height: 25,),
+
+                  //email textfield
+                  MyTextField(
+                      controller: userTextController,
+                      hintText: 'Email',
+                      obscureText: false,
+                      icon: Icon(Icons.nat),
+                      color: Colors.transparent,
+                      onTap: () {},
+                  ),
+
+                  const SizedBox(height: 10,),
+
+                  //password textfield
+                  MyTextField(
+                      controller: passwordController,
+                      hintText: 'Password',
+                      obscureText: obstext,
+                      icon: Icon(Icons.remove_red_eye),
+                      color: Colors.grey,
+                      onTap: () {
+                        setState(() {
+                          obstext = !obstext;
+                        });
+                      },
+                  ),
+
+                  const SizedBox(height: 25,),
+
+                  //singin button
+                  MyButton(
+                      onTap: singIn,
+                      text: 'Sing In'),
+
+                  const SizedBox(height: 10,),
+
+                  //go to register page
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not a member?',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue
+                            color: Colors.grey[700]
                         ),
                       ),
-                    ),
-                  ],
-                )
+                      const SizedBox(width: 4,),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: const Text(
+                          'Register Now',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
 
+                  SizedBox(height:MediaQuery.of(context).viewInsets.bottom),
 
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
--- play2learn/lib/Pages/register.dart
+++ play2learn/lib/Pages/register.dart
@@ -80,110 +80,112 @@ class _RegisterState extends State<Register> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //logo
-                Lottie.asset(
-                    'assets/animation_brain.json',
-                    repeat: false,
-                    height: 250,
-                    width: 250,
-                    animate: false
-                ),
-
-                const SizedBox(height: 50,),
-
-                //welcome back message
-                Text(
-                  "Lets create an account for you",
-                  style: TextStyle(
-                    color: Colors.grey[700],
+          child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0,),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //logo
+                  Lottie.asset(
+                      'assets/animation_brain.json',
+                      repeat: false,
+                      height: 250,
+                      width: 250,
+                      animate: false
                   ),
-                ),
 
-                const SizedBox(height: 25,),
+                  const SizedBox(height: 50,),
 
-                //email textfield
-                MyTextField(
-                    controller: userTextController,
-                    hintText: 'Email',
-                    obscureText: false,
-                    icon: Icon(Icons.nat),
-                    color: Colors.transparent,
-                    onTap: () {},
-                ),
-
-                const SizedBox(height: 10,),
-
-                //password textfield
-                MyTextField(
-                    controller: passwordController,
-                    hintText: 'Password',
-                    obscureText: obstext,
-                  icon: Icon(Icons.remove_red_eye),
-                  color: Colors.grey,
-                  onTap: () {
-                    setState(() {
-                      obstext = !obstext;
-                    });
-                  },
-                ),
-
-                const SizedBox(height: 10,),
-
-                //confirm password textfield
-                MyTextField(
-                    controller: confirmPasswordController,
-                    hintText: 'Confirm password',
-                    obscureText: true,
-                    icon: Icon(Icons.nat),
-                    color: Colors.transparent,
-                    onTap: () {},
-                ),
-
-                const SizedBox(height: 25,),
-
-                //singin button
-                MyButton(
-                    onTap: register,
-                    text: 'Sing Up'
-                ),
-
-                const SizedBox(height: 10,),
-
-                //go to register page
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have an account?',
-                      style: TextStyle(
-                          color: Colors.grey[700]
-                      ),
+                  //welcome back message
+                  Text(
+                    "Lets create an account for you",
+                    style: TextStyle(
+                      color: Colors.grey[700],
                     ),
-                    const SizedBox(width: 4,),
-                    GestureDetector(
-                      onTap: widget.onTap,
-                      child: const Text(
-                        'Login here',
+                  ),
+
+                  const SizedBox(height: 25,),
+
+                  //email textfield
+                  MyTextField(
+                      controller: userTextController,
+                      hintText: 'Email',
+                      obscureText: false,
+                      icon: Icon(Icons.nat),
+                      color: Colors.transparent,
+                      onTap: () {},
+                  ),
+
+                  const SizedBox(height: 10,),
+
+                  //password textfield
+                  MyTextField(
+                      controller: passwordController,
+                      hintText: 'Password',
+                      obscureText: obstext,
+                    icon: Icon(Icons.remove_red_eye),
+                    color: Colors.grey,
+                    onTap: () {
+                      setState(() {
+                        obstext = !obstext;
+                      });
+                    },
+                  ),
+
+                  const SizedBox(height: 10,),
+
+                  //confirm password textfield
+                  MyTextField(
+                      controller: confirmPasswordController,
+                      hintText: 'Confirm password',
+                      obscureText: true,
+                      icon: Icon(Icons.nat),
+                      color: Colors.transparent,
+                      onTap: () {},
+                  ),
+
+                  const SizedBox(height: 25,),
+
+
+                  //singin button
+                  MyButton(
+                      onTap: register,
+                      text: 'Sing Up'
+                  ),
+
+                  const SizedBox(height: 10,),
+
+                  //go to register page
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already have an account?',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue
+                            color: Colors.grey[700]
                         ),
                       ),
-                    ),
-                  ],
-                )
+                      const SizedBox(width: 4,),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: const Text(
+                          'Login here',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
 
+                  SizedBox(height:MediaQuery.of(context).viewInsets.bottom),
 
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
