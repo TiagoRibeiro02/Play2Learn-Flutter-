6bcbc98751a94bf1fef30e139c9f536f6e7c9ec6
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-09-21 19:22:12 +0100

    Update profile_page.dart

--- play2learn/lib/Pages/PagesStudents/profile_page.dart
+++ play2learn/lib/Pages/PagesStudents/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:play2learn/utils/shared_prefs.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -13,8 +14,25 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 120, height: 120,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                      child: Image(image: AssetImage(''),)),
+                ),
+                const SizedBox(height: 10,),
+                Text(UserPreferences.getUsername()),
+                Text(UserPreferences.getEmail()),
+              ],
+            ),
+          ),
+        ),
       ),
 
     );
